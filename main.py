7608ddb2a1b2c30c40a2eb2e111f677cca54375c
YuichiYH <64170401+YuichiYH@users.noreply.github.com>
2023-06-30 15:57:41 -0300

    practice gradient descent

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import numpy as np
 import torch
+def load_csv(path: str) -> torch.Tensor:
+    print("Loading CSV...")
+    data = pd.read_csv(path)
+    data = np.array(data)
+    data = np.random.shuffle(data)
 
-print("Loading CSV...")
-data = pd.read_csv('data/mnist_train.csv')
-data = np.array(data)
-data = np.random.shuffle(data)
+    print("Converting to Tensor...")
+    data = torch.tensor(data)
 
-print("Converting to Tensor...")
-data = torch.tensor(data)
+    return data
 
+
+data = load_csv('data/mnist_train.csv')
 m, n = data.size()
 
